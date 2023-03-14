8f20ef6d4279ab554ab05ab46d6927d27528e018
mspanring <markus.spanring@iarai.ac.at>
2023-03-14 17:17:40 +0100

    formatting

--- tflearning/scripts.py
+++ tflearning/scripts.py
@@ -6,7 +6,7 @@ from ml_utilities.run_utils.runner import Runner
 
 from .mode_connectivity.instability_analysis import InstabilityAnalyzer
 from .mode_connectivity.train_instability_analysis import TrainInstabilityAnalysis
-from .sample_difficulty.prediction_depth_script import PredictionDepthRunner 
+from .sample_difficulty.prediction_depth_script import PredictionDepthRunner
 
 LOGGER = logging.getLogger(__name__)
 
@@ -15,9 +15,12 @@ KEY_RUN_SCRIPT_NAME = 'run_script_name'
 KEY_RUN_SCRIPT_KWARGS = 'run_script_kwargs'
 
 
+_runner_registry = {
+    InstabilityAnalyzer.str_name: InstabilityAnalyzer,
+    TrainInstabilityAnalysis.str_name: TrainInstabilityAnalysis,
+    PredictionDepthRunner.str_name: PredictionDepthRunner
+}
 
-_runner_registry = {InstabilityAnalyzer.str_name: InstabilityAnalyzer, TrainInstabilityAnalysis.str_name: TrainInstabilityAnalysis, 
-                    PredictionDepthRunner.str_name: PredictionDepthRunner}
 
 def get_runner_script(run_script: str) -> Type[Runner]:
     if run_script in _runner_registry:
@@ -32,9 +35,10 @@ class ScriptRunner(Runner):
         OmegaConf.resolve(config)
         self.runner_script = get_runner_script(config[KEY_RUN_SCRIPT_NAME])
         self.runner = self.runner_script(**config[KEY_RUN_SCRIPT_KWARGS])
-    
+
     def run(self) -> None:
         self.runner.run()
+
 
 def run_script(cfg: DictConfig):
     LOGGER.info(f'Running script with config: \n{OmegaConf.to_yaml(cfg)}')
