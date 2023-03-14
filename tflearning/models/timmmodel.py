cea1c55049fc2464a0e57874a1b0c7d2878ca747
mspanring <markus.spanring@iarai.ac.at>
2023-03-14 16:08:31 +0100

    use proper default value

--- tflearning/models/timmmodel.py
+++ tflearning/models/timmmodel.py
@@ -5,16 +5,18 @@ from dataclasses import dataclass, field
 from ml_utilities.torch_models.base_model import BaseModel
 from tflearning.finetune_utils import prepare_model_for_finetuning
 
+
 @dataclass
 class TimmModelConfig:
     name: str = None
-    num_output_logits: Optional[int] = None
+    num_output_logits: Optional[int] = -1
     timm_kwargs: Optional[Dict[str, Any]] = field(default_factory=dict)
     freeze_layers_before: Optional[str] = ''
 
 
 class TimmModel(BaseModel):
     config_class = TimmModelConfig
+
     def __init__(self, model_cfg: Union[Dict[str, Any], TimmModelConfig]):
         super().__init__()
         if isinstance(model_cfg, dict):
@@ -25,7 +27,9 @@ class TimmModel(BaseModel):
 
     def reset_parameters(self):
         model = timm.create_model(self.config.name, **self.config.timm_kwargs)
-        self.model = prepare_model_for_finetuning(model, self.config.freeze_layers_before, self.config.num_output_logits)
+        self.model = prepare_model_for_finetuning(
+            model, self.config.freeze_layers_before, self.config.num_output_logits
+        )
 
     def forward(self, x):
         return self.model(x)
