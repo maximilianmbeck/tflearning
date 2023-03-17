b129fd805e1beb297fae345f1932bcdb8a0b8527
Maximilian Beck <maximilianbeck@live.de>
2023-03-17 12:20:32 +0100

    adopt model call convention, in order to be loadable by basemodel

--- tflearning/models/creator.py
+++ tflearning/models/creator.py
@@ -1,12 +1,13 @@
-from ml_utilities.torch_models.base_model import BaseModel
 from ml_utilities.config import ModelConfig
+from ml_utilities.torch_models.base_model import BaseModel
+
 from tflearning.models import get_model_class
 
 
 def create_model(model_cfg: ModelConfig) -> BaseModel:
     model_class = get_model_class(model_cfg.name)
 
-    model = model_class(model_cfg.kwargs)
-    # TODO: add ownjob module in creator
+    model = model_class(**model_cfg.kwargs)
+    # TODO: add ownjob module
 
     return model
--- tflearning/models/timmmodel.py
+++ tflearning/models/timmmodel.py
@@ -1,8 +1,10 @@
-from typing import Any, Dict, Optional, Union
-import timm
 from dataclasses import dataclass, field
-from omegaconf import DictConfig
+from typing import Any, Dict, Optional, Union
+
+import timm
 from ml_utilities.torch_models.base_model import BaseModel
+from omegaconf import DictConfig
+
 from tflearning.finetune_utils import prepare_model_for_finetuning
 
 
@@ -17,21 +19,23 @@ class TimmModelConfig:
 class TimmModel(BaseModel):
     config_class = TimmModelConfig
 
-    def __init__(self, model_cfg: Union[DictConfig, TimmModelConfig]):
+    def __init__(self,
+                 name: str = None,
+                 num_output_logits: Optional[int] = -1,
+                 timm_kwargs: Optional[Dict[str, Any]] = {},
+                 freeze_layers_before: Optional[str] = ''):
         super().__init__()
-        if isinstance(model_cfg, (DictConfig, dict)):
-            model_cfg = TimmModelConfig(**model_cfg)
-        self.config = model_cfg
+        self.config = TimmModelConfig(name=name,
+                                      num_output_logits=num_output_logits,
+                                      timm_kwargs=timm_kwargs,
+                                      freeze_layers_before=freeze_layers_before)
 
         self.reset_parameters()
 
     def reset_parameters(self):
         model = timm.create_model(self.config.name, **self.config.timm_kwargs)
-        self.model = prepare_model_for_finetuning(
-            model, self.config.freeze_layers_before, self.config.num_output_logits
-        )
+        self.model = prepare_model_for_finetuning(model, self.config.freeze_layers_before,
+                                                  self.config.num_output_logits)
 
     def forward(self, x):
         return self.model(x)
-
-
